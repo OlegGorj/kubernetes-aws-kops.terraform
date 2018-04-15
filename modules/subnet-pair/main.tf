ed155b637a547aaec569cee3746de8aea4e1c67d
Oleg Gorodnitchi <20544998+OlegGorj@users.noreply.github.com>
2018-04-15 15:38:43 +0200

    Update main.tf

--- modules/subnet-pair/main.tf
+++ modules/subnet-pair/main.tf
@@ -1,3 +1,5 @@
+
+
 data "aws_availability_zone" "az" {
   count = "${length(var.availability_zones)}"
   name  = "${var.availability_zones[count.index]}"
@@ -161,3 +163,33 @@ resource "aws_security_group_rule" "nat_egress" {
     create_before_destroy = true
   }
 }
+
+
+
+
+
+
+output "public_subnet_ids" {
+  value = ["${aws_subnet.public.*.id}"]
+}
+
+output "private_subnet_ids" {
+  value = ["${aws_subnet.private.*.id}"]
+}
+
+output "default_security_group_id" {
+  value = "${aws_security_group.default.id}"
+}
+
+output "nat_gateway_ids" {
+  value = ["${aws_nat_gateway.nat_gw.*.id}"]
+}
+
+output "nat_security_group_id" {
+  value = "${aws_security_group.nat.id}"
+}
+
+output "availability_zones" {
+  value = "${var.availability_zones}"
+}
+
