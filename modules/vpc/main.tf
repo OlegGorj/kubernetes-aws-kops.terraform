6d45163229717b1159d8e57f4287c13eca05c56e
Oleg Gorodnitchi <20544998+OlegGorj@users.noreply.github.com>
2018-04-15 15:42:24 +0200

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,7 @@
+//
+//  this tf module is to be replaced with generic vpc module
+//
+
 resource "aws_vpc" "default" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true
@@ -7,4 +11,19 @@ resource "aws_vpc" "default" {
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
   tags   = "${merge(map("Name", "${var.name}-${var.env}-ig"), var.tags)}"
+}
+
+
+
+
+output "vpc_id" {
+  value = "${aws_vpc.default.id}"
+}
+
+output "cidr_block" {
+  value = "${aws_vpc.default.cidr_block}"
+}
+
+output "internet_gateway_id" {
+  value = "${aws_internet_gateway.default.id}"
 }
